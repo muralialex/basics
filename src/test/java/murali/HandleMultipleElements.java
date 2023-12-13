e3363082ec10ca4a1b4a47766a224fc394e95ca3
Murali Sukumar <unique.muralistl@gmail.com>
2023-12-13 08:41:07 +0530

    Multiple Elements and Screenshot code added

---
+++ src/test/java/murali/HandleMultipleElements.java
@@ -0,0 +1,26 @@
+package murali;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class HandleMultipleElements {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		((JavascriptExecutor)driver).executeScript("document.body.style.zoom='80%';");
+		Thread.sleep(5000);
+		List<WebElement> elements = driver.findElements(By.xpath("//div[@id='nav-xshop']/a[position()<=15]"));
+		for(WebElement element: elements) {
+			System.out.println("Text=> "+element.getText());
+			System.out.println("Attribute=> " + element.getAttribute("data-csa-c-content-id"));
+		}
+	}
+}
---
+++ src/test/java/murali/HandleScreenshot.java
@@ -0,0 +1,35 @@
+package murali;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class HandleScreenshot {
+
+	public static void main(String[] args) throws IOException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://online.actitime.com/cisco2");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		File actiTimeFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File destFile=new File(System.getProperty("user.dir")+"\\resources\\screenshots\\actiTime_Login.png");
+		FileUtils.copyFile(actiTimeFile, destFile);
+		
+		driver.navigate().to("https://www.amazon.in/");
+		File amazonFile = driver.findElement(By.tagName("html")).getScreenshotAs(OutputType.FILE);
+		destFile=new File(System.getProperty("user.dir")+"\\resources\\screenshots\\amazon_home.png");
+		FileUtils.copyFile(amazonFile, destFile);
+		
+		File amazon_elementFile = driver.findElement(By.xpath("//div[@id='nav-xshop']")).getScreenshotAs(OutputType.FILE);
+		destFile=new File(System.getProperty("user.dir")+"\\resources\\screenshots\\amazon_element.png");
+		FileUtils.copyFile(amazon_elementFile, destFile);
+	}
+
+}
